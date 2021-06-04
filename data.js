993830765a064602520d2d1439a6f3d36fde7868
vrkshetty <vijayrkshetty@gmail.com>
2021-06-05 00:23:56 +0100

    first commi

---
+++ data.js
@@ -0,0 +1,81 @@
+var myList = [
+  {
+    varase: "Sarala Varase",
+    Number: "1.",
+    Text: " Sa Ri Ga Ma | Pa Da | Ni Sa || </br></br> Sa Ni Da Pa | Ma Ga | Ri Sa ||",
+    audoSrc: "sounds/1.mp3",
+  },
+  {
+    Number: "2.",
+    Text: " Sa Ri Ga Ma | Sa Ri | Ga Ma ||</br></br> Sa Ri Ga Ma | Pa Da | Ni Sa || </br></br></br> Sa Ni Da Pa | Sa Ni | Da Pa || </br> </br> Sa Ni Da Pa | Ma Ga | Ri Sa ||",
+    audoSrc: "sounds/2.mp3",
+  },
+  {
+    Number: "3.",
+    Text: " Sa Ri Ga Ma | Sa Ri | Sa Ri || </br> </br>  Sa Ri Ga Ma | Pa Da | Ni Sa || </br></br> </br> Sa Ni Da Pa | Sa Ni | Sa Ni || </br> </br> Sa Ni Da Pa | Ma Ga | Ri Sa ||",
+    audoSrc: "sounds/3.mp3",
+  },
+  {
+    Number: "4.",
+    Text: "Sa Ri Ga Sa | Ri Ga | Sa Ri || </br> </br> Sa Ri Ga Ma | Pa Da | Ni Sa || </br> </br></br> Sa Ni Da Sa | Ni Da | Sa Ni || </br> </br> Sa Ni Da Pa | Ma Ga | Ri Sa ||",
+    audoSrc: "sounds/4.mp3",
+  },
+  {
+    Number: "5.",
+    Text: "Sa Ri Ga Ma | Paa | Sa Ri ||</br> </br> Sa Ri Ga Ma | Pa Da | Ni Sa || </br></br> </br> Sa Ni Da Pa | Maa | Sa Ni || </br> </br> Sa Ni Da Pa | Ma Ga | Ri Sa ||",
+    audoSrc: "sounds/5.mp3",
+  },
+  {
+    Number: "6.",
+    Text: "Sa Ri Ga Ma | Pa Ma | Ga Ri ||</br> </br> Sa Ni Da Pa | Ma Pa | Da Ni || </br></br> </br> Sa Ni Da Pa | Maa | Sa Ni || </br> </br> Sa Ni Da Pa | Ma Ga | Ri Sa ||",
+    audoSrc: "sounds/6.mp3",
+  },
+  {
+    Number: "7.",
+    Text: "Sa Ri Ga Ma | Pa Ma | Da Pa ||</br> </br> Sa Ri Ga Ma | Pa Da | Ni Sa || </br></br> </br> Sa Ni Da Pa | Ma Pa | Ga Ma || </br> </br> Sa Ni Da Pa | Ma Ga | Ri Sa ||",
+    audoSrc: "sounds/7.mp3",
+  },
+
+  {
+    varase: "Taara Sthayee Varase",
+    Number: "1.",
+    Text: "Sa Ri | Ga Ma | Pa Da | Ni Sa ||</br> </br> Saaa......| Saaa......|| </br></br> Da Ni Sa Ri | Sa Ni | Da Pa || </br> </br> Sa Ni Da Pa | Ma Ga | Ri Sa ||",
+    audoSrc: "sounds/8.mp3",
+  },
+  {
+    Number: "2.",
+    Text: "Sa Ri | Ga Ma | Pa Da | Ni Sa ||</br> </br> Saaa......| Saaa......|| </br></br> Da Ni Sa Ri | Sa Sa | Ri Sa || </br> </br> Sa Ri Sa Ni | Da Pa | Ma Pa || </br> </br>Da Ni Sa Ri | Sa Ni | Da Pa ||</br> </br>Sa Ni Da Pa | Ma Ga | Ri Sa ||",
+    audoSrc: "sounds/9.mp3",
+  },
+  {
+    Number: "3.",
+    Text: "Sa Ri | Ga Ma | Pa Da | Ni Sa ||</br> </br> Saaa......| Saaa......|| </br></br> Da Ni Sa Ri | Ga Ri | Sa Ri || </br> </br> Sa Ri Sa Ni | Da Pa | Ma Pa || </br> </br>Da Ni Sa Ri | Sa Sa | Ri Sa ||</br> </br>Sa Ri Sa Ni | Da Pa | Ma Pa ||</br> </br>Da Ni Sa Ri | Sa Ni | Da Pa ||</br> </br>Sa Ni Da Pa | Ma Ga | Ri Sa ||",
+    audoSrc: "sounds/10.mp3",
+  },
+  {
+    Number: "4.",
+    Text: "Sa Ri | Ga Ma | Pa Da | Ni Sa ||</br> </br>Saaa...... | Saaa...... ||</br> </br>Da Ni Sa Ri | Ga Ma | Ga Ri ||</br> </br>Sa Ri Sa Ni | Da Pa | Ma Pa ||</br> </br>Da Ni Sa Ri | Ga Ri | Sa Ri ||</br> </br>Sa Ri Sa Ni | Da Pa | Ma Pa ||</br> </br>Da Ni Sa Ri | Sa Sa | Ri Sa ||</br> </br>Sa Ri Sa Ni | Da Pa | Ma Pa ||</br> </br>Da Ni Sa Ri | Sa Ni | Da Pa ||</br> </br>Sa Ni Da Pa | Ma Ga | Ri Sa ||",
+    audoSrc: "sounds/11.mp3",
+  },
+  {
+    varase: "Janti Varase",
+    Number: "1.",
+    Text: "SaSa RiRi GaGa MaMa </br> </br> PaPa DaDa NiNi SaSa || </br> </br> SaSa NiNi DaDa PaPa</br> </br> MaMa GaGa RiRi SaSa ||  ",
+    audoSrc: "sounds/Janti_1.mp3",
+  },
+  {
+    Number: "2.",
+    Text: "SaSa RiRi GaGa MaMa || </br> </br> RiRi GaGa MaMa PaPa || </br> </br> GaGa MaMa PaPA DaDa </br> </br> MaMa PaPa DaDa NiNi || </br></br> PaPa DaDa NiNi SaSa </br></br>SaSa NiNi DaDa PaPa </br></br> NiNi DaDa PaPa MaMa</br></br> DaDa PaPa MaMa GaGa </br></br>PaPa MaMa GaGa RiRi </br></br>MaMa GaGa RiRi SaSa  ",
+    audoSrc: "sounds/Janti_2.mp3",
+  },
+  {
+    Number: "3.",
+    Text: "SaSa RiRi GaGa RiRi || </br> </br> SaSa RiRi GaGa MaMa  || </br> </br> RiRi GaGa MaMa GaGa || </br> </br> RiRi GaGa MaMa PaPa || </br></br> GaGa MaMa PaPa MaMa </br></br>GaGa MaMa PaPa DaDa</br></br>MaMa PaPa DaDa PaPa</br></br>MaMa PaPa DaDa NiNi </br></br>PaPa DaDa NiNi DaDa</br></br>PaPa DaDa NiNi SaSa</br></br>SaSa NiNi DaDa NiNi</br></br>SaSa NiNi DaDa PaPa</br></br>NiNi DaDa PaPa DaDa</br></br>NiNi DaDa PaPa MaMa</br></br>DaDa PaPa MaMa PaPa</br></br>DaDa PaPa MaMa GaGa</br></br>PaPa MaMa GaGa MaMa</br></br>PaPa MaMa GaGa RiRi </br></br>MaMa GaGa RiRi GaGa </br></br> MaMa GaGa RiRi SaSa",
+    audoSrc: "sounds/Janti_3.mp3",
+  },
+  {
+    Number: "4. Not Done",
+    Text: "SaSa RiRi GaGa RiRi || </br> </br> SaSa RiRi GaGa MaMa  || </br> </br> RiRi GaGa MaMa GaGa || </br> </br> RiRi GaGa MaMa PaPa || </br></br> GaGa MaMa PaPa MaMa </br></br>GaGa MaMa PaPa DaDa</br></br>MaMa PaPa DaDa PaPa</br></br>MaMa PaPa DaDa NiNi </br></br>PaPa DaDa NiNi DaDa</br></br>PaPa DaDa NiNi SaSa</br></br>SaSa NiNi DaDa NiNi</br></br>SaSa NiNi DaDa PaPa</br></br>NiNi DaDa PaPa DaDa</br></br>NiNi DaDa PaPa MaMa</br></br>DaDa PaPa MaMa PaPa</br></br>DaDa PaPa MaMa GaGa</br></br>PaPa MaMa GaGa MaMa</br></br>PaPa MaMa GaGa RiRi </br></br>MaMa GaGa RiRi GaGa </br></br> MaMa GaGa RiRi SaSa",
+    audoSrc: "sounds/8.mp3",
+  },
+];
