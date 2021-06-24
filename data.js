a7c81fc28b5641640a91d8b6d08c14055b256da6
vrkshetty <vijayrkshetty@gmail.com>
2021-06-24 15:13:53 +0100

    added4

--- data.js
+++ data.js
@@ -79,4 +79,9 @@ var myList = [
     Text: "SaSa RiRi Ga SaRiGa || </br> </br> SaSa RiRi GaGa MaMa  || </br> </br> RiRi GaGa Ma RiGaMa || </br> </br> RiRi GaGa MaMa PaPa || </br></br> GaGa MaMa Pa GaMaPa </br></br>GaGa MaMa PaPa DaDa</br></br>MaMa PaPa Da MaPaDa ||</br></br>MaMa PaPa DaDa NiNi ||</br></br>PaPa DaDa Ni PaDaNi |</br></br>PaPa DaDa NiNi SaSa ||</br></br>SaSa NiNi Da SaNiDa |</br></br>SaSa NiNi DaDa PaPa</br></br>NiNi DaDa Pa NiDaPa</br></br>NiNi DaDa PaPa MaMa</br></br>DaDa PaPa Ma DaPaMa</br></br>DaDa PaPa MaMa GaGa</br></br>PaPa MaMa Ga PaMaGa |</br></br>PaPa MaMa GaGa RiRi ||</br></br>MaMa GaGa Ri MaGaRi </br></br> MaMa GaGa RiRi SaSa ||",
     audoSrc: "sounds/8.mp3",
   },
+  {
+    Number: "5.",
+    Text: "|| Sa Sa Ri Sa Sa Ri Sa Ri | Sa Sa Ri Ri | Ga Ga Ma Ma || </br> </br> || Ri Ri Ga Ri Ri Ga Ri Ga | Ri Ri Ga Ga | Ma Ma Pa Pa || </br> </br> || Ga Ga Ma Ga Ga Ma Ga Ma | ||  Ga Ga Ma Ma | Pa Pa Da Da || </br> </br> || Ma Ma Pa Ma Ma Pa Ma Pa | Ma Ma Pa Pa | Da Da Ni Ni || </br></br> Pa Pa Da Pa Pa Da Pa Da ||| Pa Pa Da Da Ni Ni Sa+ Sa+ || </br></br>Sa+ Sa+ Ni Sa+ Sa+ Ni Sa+ Ni | Sa+ Sa+ Ni Ni | Da Da Pa Pa ||</br></br>|| Ni Ni Da Ni Ni Da Ni Da || Ni Ni Da Da | Pa Pa Ma Ma ||</br></br>|| Da Da Pa Da Da Pa Da Pa || Da Da Pa Pa | Ma Ma Ga Ga ||</br></br>|| Pa Pa Ma Pa Pa Ma Pa Ma | Pa Pa Ma Ma | Ga Ga Ri Ri|| </br></br>|| Ma Ma Ga Ma Ma Ga Ma Ga | Ma Ma Ga Ga | Ri Ri Sa Sa ||",
+    audoSrc: "sounds/8.mp3",
+  },
 ];
